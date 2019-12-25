61f2016c98403c3819f83e12954b49e8bea51e09
hs14231 <59163642+hs14231@users.noreply.github.com>
2019-12-25 13:21:11 +0900

    不要なコメントを削除

--- FileCreater/FileCreater.cpp
+++ FileCreater/FileCreater.cpp
@@ -1,5 +1,5 @@
 
-// FileCreater.cpp : �A�v���P�[�V�����̃N���X������`���܂��B
+// FileCreater.cpp : アプリケーションのクラス動作を定義します。
 //
 
 #include "stdafx.h"
@@ -18,34 +18,34 @@ BEGIN_MESSAGE_MAP(CFileCreaterApp, CWinApp)
 END_MESSAGE_MAP()
 
 
-// CFileCreaterApp �R���X�g���N�V����
+// CFileCreaterApp コンストラクション
 
 CFileCreaterApp::CFileCreaterApp()
 {
-	// �ċN���}�l�[�W���[���T�|�[�g���܂�
+	// 再起動マネージャーをサポートします
 	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
 
-	// TODO: ���̈ʒu�ɍ\�z�p�R�[�h��ǉ����Ă��������B
-	// ������ InitInstance ���̏d�v�ȏ��������������ׂċL�q���Ă��������B
+	// TODO: この位置に構築用コードを追加してください。
+	// ここに InitInstance 中の重要な初期化処理をすべて記述してください。
 }
 
 
-// �B��� CFileCreaterApp �I�u�W�F�N�g�ł��B
+// 唯一の CFileCreaterApp オブジェクトです。
 
 CFileCreaterApp theApp;
 
 
-// CFileCreaterApp ������
+// CFileCreaterApp 初期化
 
 BOOL CFileCreaterApp::InitInstance()
 {
-	// �A�v���P�[�V���� �}�j�t�F�X�g�� visual �X�^�C����L���ɂ��邽�߂ɁA
-	// ComCtl32.dll Version 6 �ȍ~�̎g�p���w�肷��ꍇ�́A
-	// Windows XP �� InitCommonControlsEx() ���K�v�ł��B�����Ȃ���΁A�E�B���h�E�쐬�͂��ׂĎ��s���܂��B
+	// アプリケーション マニフェストが visual スタイルを有効にするために、
+	// ComCtl32.dll Version 6 以降の使用を指定する場合は、
+	// Windows XP に InitCommonControlsEx() が必要です。さもなければ、ウィンドウ作成はすべて失敗します。
 	INITCOMMONCONTROLSEX InitCtrls;
 	InitCtrls.dwSize = sizeof(InitCtrls);
-	// �A�v���P�[�V�����Ŏg�p���邷�ׂẴR���� �R���g���[�� �N���X���܂߂�ɂ́A
-	// �����ݒ肵�܂��B
+	// アプリケーションで使用するすべてのコモン コントロール クラスを含めるには、
+	// これを設定します。
 	InitCtrls.dwICC = ICC_WIN95_CLASSES;
 	InitCommonControlsEx(&InitCtrls);
 
@@ -54,42 +54,40 @@ BOOL CFileCreaterApp::InitInstance()
 
 	AfxEnableControlContainer();
 
-	// �_�C�A���O�ɃV�F�� �c���[ �r���[�܂��̓V�F�� ���X�g �r���[ �R���g���[����
-	// �܂܂�Ă���ꍇ�ɃV�F�� �}�l�[�W���[���쐬���܂��B
+	// ダイアログにシェル ツリー ビューまたはシェル リスト ビュー コントロールが
+	// 含まれている場合にシェル マネージャーを作成します。
 	CShellManager *pShellManager = new CShellManager;
 
-	// �W��������
-	// �����̋@�\���g�킸�ɍŏI�I�Ȏ��s�\�t�@�C����
-	// �T�C�Y���k���������ꍇ�́A�ȉ�����s�v�ȏ�����
-	// ���[�`�����폜���Ă��������B
-	// �ݒ肪�i�[����Ă��郌�W�X�g�� �L�[��ύX���܂��B
-	// TODO: ��Ж��܂��͑g�D���Ȃǂ̓K�؂ȕ������
-	// ���̕������ύX���Ă��������B
-	SetRegistryKey(_T("�A�v���P�[�V���� �E�B�U�[�h�Ő������ꂽ���[�J�� �A�v���P�[�V����"));
+	// 標準初期化
+	// これらの機能を使わずに最終的な実行可能ファイルの
+	// サイズを縮小したい場合は、以下から不要な初期化
+	// ルーチンを削除してください。
+	// 設定が格納されているレジストリ キーを変更します。
+	// TODO: 会社名または組織名などの適切な文字列に
+	// この文字列を変更してください。
+	SetRegistryKey(_T("アプリケーション ウィザードで生成されたローカル アプリケーション"));
 
 	CFileCreaterDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
 	if (nResponse == IDOK)
 	{
-		// TODO: �_�C�A���O�� <OK> �ŏ����ꂽ���̃R�[�h��
-		//  �L�q���Ă��������B
+		// TODO: ダイアログが <OK> で消された時のコードを
+		//  記述してください。
 	}
 	else if (nResponse == IDCANCEL)
 	{
-		// TODO: �_�C�A���O�� <�L�����Z��> �ŏ����ꂽ���̃R�[�h��
-		//  �L�q���Ă��������B
+		// TODO: ダイアログが <キャンセル> で消された時のコードを
+		//  記述してください。
 	}
 
-	// ��ō쐬���ꂽ�V�F�� �}�l�[�W���[���폜���܂��B
+	// 上で作成されたシェル マネージャーを削除します。
 	if (pShellManager != NULL)
 	{
 		delete pShellManager;
 	}
 
-	// �_�C�A���O�͕����܂����B�A�v���P�[�V�����̃��b�Z�[�W �|���v���J�n���Ȃ���
-	//  �A�v���P�[�V�������I�����邽�߂� FALSE ��Ԃ��Ă��������B
+	// ダイアログは閉じられました。アプリケーションのメッセージ ポンプを開始しないで
+	//  アプリケーションを終了するために FALSE を返してください。
 	return FALSE;
 }
-
-//sfddsfaaaaaaaaaaaaaaaaaaaaaaaaaaa
